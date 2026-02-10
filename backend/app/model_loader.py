7d1edd8520d087e74679285e8f224412cde2a07b
ghanashyamd12 <reachghanshyamd1@gmail.com>
2026-02-10 20:25:32 +0530

    Fix scaler loading

--- backend/app/model_loader.py
+++ backend/app/model_loader.py
@@ -1,10 +1,13 @@
 import joblib
 import os
 
-MODEL_PATH = os.path.join(
-    os.path.dirname(__file__),
-    "../../ml/model/rul_model.pkl"
-)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+MODEL_PATH = os.path.join(BASE_DIR, "ml", "model", "rul_model.pkl")
+SCALER_PATH = os.path.join(BASE_DIR, "ml", "model", "scaler.pkl")
+
 
 def load_model():
-    return joblib.load(MODEL_PATH)
+    model = joblib.load(MODEL_PATH)
+    scaler = joblib.load(SCALER_PATH)
+    return model, scaler
